65b18aa06f36fac4ac2e7e9e1c660c5dfe3b3b73
IMJGyeongMin <94818108+IMJGyeongMin@users.noreply.github.com>
2021-12-07 15:37:37 +0900

    2021-12-07-3 (Artists Tap, Protect Works update)

---
+++ src/js/Main.js
@@ -0,0 +1,35 @@
+var keydownCtrl = 0;
+var keydownShift = 0;
+
+document.onkeydown = keycheck;
+document.onkeyup = uncheckCtrlShift;
+
+function keycheck() {
+  switch (event.keyCode) {
+    case 123:
+      event.keyCode = "";
+      return false;
+      break; //F12
+    case 17:
+      event.keycode = "";
+      keydownCtrl = 1;
+      return false;
+      break; //Ctrl
+  }
+
+  if (keydownCtrl) return false;
+}
+
+function uncheckCtrlShift() {
+  if (event.keyCode == 17) keydownCtrl = 0;
+  if (event.KeyCode == 16) keydownShift = 0;
+}
+
+function click() {
+  if (event.button == 2 || event.button == 2) {
+    alert(
+      "Right-click, drag, and developer tools are prohibited to protect the work."
+    );
+  }
+}
+document.onmousedown == click;
