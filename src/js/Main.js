0304e260bb50e2973c99a293fb2b303cddba40cc
gyeongent <59150207+gyeongent@users.noreply.github.com>
2022-03-23 15:54:00 +0900

    protect works

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,62 +1,18 @@
-/*
-var keydownCtrl = 0;
-var keydownShift = 0;
-
-document.onkeydown = keycheck;
-document.onkeyup = uncheckCtrlShift;
-
-function keycheck() {
-  switch (event.keyCode) {
-    case 123:
-      event.keyCode = "";
-      return false;
-      break; //F12
-    case 17:
-      event.keycode = "";
-      keydownCtrl = 1;
-      return false;
-      break; //Ctrl
-
-      alert(
-        "Right-click, drag, and developer tools are prohibited to protect the work."
-      );
-  }
-
-  if (keydownCtrl) return false;
-  alert(
-    "Right-click, drag, and developer tools are prohibited to protect the work."
-  );
-}
-
-function uncheckCtrlShift() {
-  if (event.keyCode == 17) keydownCtrl = 0;
-  if (event.KeyCode == 16) keydownShift = 0;
-  alert(
-    "Right-click, drag, and developer tools are prohibited to protect the work."
-  );
-}
-
-function click() {
-  if (event.button == 2 || event.button == 2) {
-    alert(
-      "Right-click, drag, and developer tools are prohibited to protect the work."
-    );
-  }
-}
-
-document.onmousedown == click;
-
-$(document).bind("contextmenu", function (e) {
-  alert(
-    "Right-click, drag, and developer tools are prohibited to protect the work."
-  );
+document.oncontextmenu = function (e){
+  alert('Do not right-click the mouse to protect works.');
   return false;
-});
+}
 
-const toggleBtn = document.querySelector("#nav-icon4");
-const menu = document.querySelector("#mobile-menu");
+document.ondragstart = function (e){
+  alert('Drag is prohibited to protect works.');
+  return false;
+}
 
-toggleBtn.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
-*/
+document.onkeydown = function(e) {
+  if(e.keyCode === 123) {
+    e.preventDefault();
+    e.returnValue = false;
+
+    alert('Developer mode is prohibited to protect works.')
+  }
+}
